72620a41f03c307f48f046fd48077bdcdc95656a
karmajunkie <keith.gaddis@gmail.com>
2014-09-25 17:21:55 -0500

    updated spec on report columns

--- spec/lib/tabloid/report_column_spec.rb
+++ spec/lib/tabloid/report_column_spec.rb
@@ -1,46 +1,11 @@
 require "spec_helper"
 
 describe Tabloid::ReportColumn do
-
-  class Formatter
-    def format_for_value(value)
-      "Formatted value #{value}"
-    end
-
-    def format_for_value_and_row(value, row)
-      "Value #{value}, Row #{row.join " "}"
-    end
-
-    def format_with_incorrect_arity
-      ""
-    end
+  it "converts key to a string" do
+    expect(Tabloid::ReportColumn.new(:foo).key).to eq('foo')
   end
 
-  describe "#initialize" do
-    context "[invalid formatting options]"do
-      it "should raise exception when missing formatting_by" do
-        expect do
-          Tabloid::ReportColumn.new(:col, "Column", :formatter => :format_for_value, :formatting_by => nil)
-        end.should raise_error(Tabloid::ReportColumn::FormatterError, "formatter or formatting_by is not specified")
-      end
-      it "should raise exception when formatter is incorrect" do
-        expect do
-          Tabloid::ReportColumn.new(:col, "Column", :formatter => :incorrect_format, :formatting_by => Formatter.new)
-        end.should raise_error(Tabloid::ReportColumn::FormatterError, "formatter method doesn't supported by formatting_by")
-      end
-    end
-  end
-
-  describe "#format" do
-    it "should raise exception when formatter arity equal 0 or greater then 2" do
-      expect do
-        Tabloid::ReportColumn.new(:col, "Column", :formatter => :format_with_incorrect_arity, :formatting_by => Formatter.new)
-      end.should raise_error(Tabloid::ReportColumn::FormatterError, "Incorrect formatter arity: 0")
-    end
-
-    it "should use custom format" do
-      col = Tabloid::ReportColumn.new(:col, "Column", :formatter => :format_for_value, :formatting_by => Formatter.new)
-      col.format(1, []).should == "Formatted value 1"
-    end
+  it "humanizes implied labels" do
+    expect(Tabloid::ReportColumn.new(:fooBar).label).to eq("Foo Bar")
   end
 end
